a8c13788f41048f9dadb4e4619d8d408f9424398
hiratayume <yume.hirata@rakus.co.jp>
2018-11-07 16:17:25 +0900

    (5-1)SQLインジェクション脆弱性を修正

--- src/main/java/jp/co/rakus/stockmanagement/repository/MemberRepository.java
+++ src/main/java/jp/co/rakus/stockmanagement/repository/MemberRepository.java
@@ -39,12 +39,11 @@ public class MemberRepository {
 	 * @return メンバー情報.メンバーが存在しない場合はnull.
 	 */
 	public Member findByMailAddressAndPassword(String mailAddress, String password) {
-		SqlParameterSource param = new MapSqlParameterSource();
+		SqlParameterSource param = new MapSqlParameterSource().addValue("mailAddress", mailAddress).addValue("password", password);
 		Member member = null;
 		try{
 			member = jdbcTemplate.queryForObject(
-					"SELECT id,name,mail_address,password FROM members WHERE mail_address= '"
-							+ mailAddress + "' and password='" + password + "'",
+					"SELECT id,name,mail_address,password FROM members WHERE mail_address=:mailAddress and password=:password",
 					param, 
 					MEMBER_ROW_MAPPER);
 			return member;
